95c773fab4c2cf06a1916f096df3bb41178b0791
abdifatah <abdifatahshukri15@gmail.com>
2023-08-21 15:46:45 +0300

    updated

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,6 +3,20 @@ import React, { useState } from "react";
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining(prevTime => {
+        if (prevTime > 1) {
+          return prevTime - 1;
+        } else {
+          clearInterval(timer);
+          handleAnswer(false); 
+          return 10;
+        }
+      });
+    }, 1000);
+  })
+
   // add useEffect code
 
   function handleAnswer(isCorrect) {
